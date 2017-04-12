ebc927cac8ce0587477d8ec96eae9f8833c97460
xiuyuhe <xyhe@qingyuanos.com>
2017-04-12 22:05:21 +0800

    add security

--- src/main/java/com/bupt/controller/DemoController.java
+++ src/main/java/com/bupt/controller/DemoController.java
@@ -2,6 +2,7 @@ package com.bupt.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by hexiuyu on 2017/4/11.
@@ -9,7 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class DemoController {
     @RequestMapping("/demo")
-    public String demoPage(){
-        return null;
+    public @ResponseBody String demoPage(){
+        return "demo";
     }
+
+
 }
