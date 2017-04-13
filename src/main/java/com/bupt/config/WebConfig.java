3b3d25196577de8d1fece3bfe5ccee5d893137ad
xiuyuhe <xyhe@qingyuanos.com>
2017-04-13 12:47:14 +0800

    add security

--- src/main/java/com/bupt/config/WebConfig.java
+++ src/main/java/com/bupt/config/WebConfig.java
@@ -14,6 +14,7 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/home").setViewName("home");
         //registry.addViewController("/").setViewName("home");
+        registry.addRedirectViewController("/", "/pages/patrol.html");
         registry.addViewController("/hello").setViewName("hello");
         registry.addViewController("/login").setViewName("login");
     }
--- src/main/java/com/bupt/config/WebSecurityConfig.java
+++ src/main/java/com/bupt/config/WebSecurityConfig.java
@@ -7,12 +7,18 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import javax.sql.DataSource;
+
 /**
  * Created by hexiuyu on 2017/4/12.
  */
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -30,8 +36,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER");
+
     }
 }
