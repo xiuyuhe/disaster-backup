4b6bb0e94750089f4e79fafd01b421450b68d55b
xiuyuhe <xyhe@qingyuanos.com>
2017-04-13 14:23:22 +0800

    add role info

--- src/main/java/com/bupt/config/WebSecurityConfig.java
+++ src/main/java/com/bupt/config/WebSecurityConfig.java
@@ -1,11 +1,18 @@
 package com.bupt.config;
 
+import com.bupt.service.UserInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.userdetails.UserDetailsServiceConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import javax.sql.DataSource;
 
@@ -39,5 +46,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         auth
                 .inMemoryAuthentication()
                 .withUser("user").password("123456").roles("USER");
+    }
+
+
+    @Service("MyUserDetailsImpl")
+    public class CustomUserDetailsService implements UserDetailsService {
+
+        @Autowired
+        private UserInfoService userInfoService;
+
+
+
+        @Override
+        public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+            return null;
+        }
     }
 }
---
+++ src/main/java/com/bupt/domain/RoleInfo.java
@@ -0,0 +1,66 @@
+package com.bupt.domain;
+
+import javax.persistence.*;
+
+/**
+ * Created by hexiuyu on 2017/4/13.
+ */
+@Entity
+@Table(name = "role_info", schema = "disaster-recovery")
+public class RoleInfo {
+    private String id;
+    private String roleName;
+    private String authority;
+
+    @Id
+    @Column(name = "ID", nullable = false, length = 32)
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "ROLE_NAME", nullable = true, length = 16)
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    @Basic
+    @Column(name = "AUTHORITY", nullable = true, length = 16)
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        RoleInfo roleInfo = (RoleInfo) o;
+
+        if (id != null ? !id.equals(roleInfo.id) : roleInfo.id != null) return false;
+        if (roleName != null ? !roleName.equals(roleInfo.roleName) : roleInfo.roleName != null) return false;
+        if (authority != null ? !authority.equals(roleInfo.authority) : roleInfo.authority != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (roleName != null ? roleName.hashCode() : 0);
+        result = 31 * result + (authority != null ? authority.hashCode() : 0);
+        return result;
+    }
+}
