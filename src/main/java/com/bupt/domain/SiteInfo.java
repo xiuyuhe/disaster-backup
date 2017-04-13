cbec36521dd05dbab2977ac26df60d78d2fa7981
caijen <caijen@10.101.164.165>
2017-04-13 16:52:06 +0800

    yaosiyu

---
+++ src/main/java/com/bupt/domain/SiteInfo.java
@@ -0,0 +1,113 @@
+package com.bupt.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+/**
+ * 
+ * @author yaosiyu
+ *
+ */
+
+@Entity
+@Table(name = "site_info")
+public class SiteInfo {
+	private String id;
+	private String code;
+	private String name;
+	private String stationMaster;
+	private String personNumber;
+	private String propertyNumber;
+	private String chargeUnit;
+	private String carNumber;
+	private String status;
+	
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid")
+    @Column(name="ID", nullable = false)
+	public String getId() {
+		return id;
+	}
+    
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+    @Column(name = "CODE")
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+	
+	@Column(name = "NAME")
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Column(name = "STATION_MASTER")
+	public String getStationMaster() {
+		return stationMaster;
+	}
+
+	public void setStationMaster(String stationMaster) {
+		this.stationMaster = stationMaster;
+	}
+	
+	@Column(name = "PERSON_NUMBER")
+	public String getPersonNumber() {
+		return personNumber;
+	}
+
+	public void setPersonNumber(String personNumber) {
+		this.personNumber = personNumber;
+	}
+	
+	@Column(name = "PROPERTY_NUMBER")
+	public String getPropertyNumber() {
+		return propertyNumber;
+	}
+
+	public void setPropertyNumber(String propertyNumber) {
+		this.propertyNumber = propertyNumber;
+	}
+	
+	@Column(name = "CHARGE_UNIT")
+	public String getChargeUnit() {
+		return chargeUnit;
+	}
+
+	public void setChargeUnit(String chargeUnit) {
+		this.chargeUnit = chargeUnit;
+	}
+	
+	@Column(name = "CAR_NUMBER")
+	public String getCarNumber() {
+		return carNumber;
+	}
+
+	public void setCarNumber(String carNumber) {
+		this.carNumber = carNumber;
+	}
+	
+	@Column(name = "STATUS")
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+}
