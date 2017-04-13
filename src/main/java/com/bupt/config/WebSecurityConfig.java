9946199b24d888f2a0eda3b2eae4b88a3b02e332
xiuyuhe <xyhe@qingyuanos.com>
2017-04-13 13:19:47 +0800

    add security  pass

--- src/main/java/com/bupt/config/WebSecurityConfig.java
+++ src/main/java/com/bupt/config/WebSecurityConfig.java
@@ -36,6 +36,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("123456").roles("USER");
     }
 }
