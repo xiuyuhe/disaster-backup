feacdfa37f25b34d3ca7763e34e363720be4f17d
xiuyuhe <xyhe@qingyuanos.com>
2017-04-13 17:44:52 +0800

    添加权限控制

--- src/main/java/com/bupt/config/WebSecurityConfig.java
+++ src/main/java/com/bupt/config/WebSecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -36,6 +37,7 @@ import java.util.List;
  */
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -138,12 +140,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         }
     }
 
-
-
-    public class MyDaoAuthenticationProvider extends DaoAuthenticationProvider{
-
-
-    }
 
     @Component
     public class MyAuthenticationProvider implements AuthenticationProvider {
