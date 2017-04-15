8a4aed0bcbeefe0d70da2f3f2eab65cdb12ae982
wang <18301561971@163.com>
2017-04-15 15:24:20 +0800

    增加PatrolPotential分页方法

--- src/main/java/com/bupt/service/PatrolPotentialService.java
+++ src/main/java/com/bupt/service/PatrolPotentialService.java
@@ -1,25 +1,38 @@
 package com.bupt.service;
 
+import com.bupt.common.base.BasePageService;
+import com.bupt.common.base.PageEntity;
 import com.bupt.domain.PatrolPotential;
 import com.bupt.repository.PatrolPotentialRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Map;
+
 /**
  * Created by bupt626 on 17-4-12.
  */
 @Service
 @Transactional
-public class PatrolPotentialService {
+public class PatrolPotentialService extends BasePageService {
     @Autowired
     private PatrolPotentialRepository repository;
 
-    public void save(PatrolPotential entity){
+    public void save(PatrolPotential entity) {
         repository.save(entity);
     }
-    public void findOne(String id){
+
+    public void findOne(String id) {
         repository.findOne(id);
     }
 
+    public void pageByHql(PageEntity<PatrolPotential> pageEntity, Map<String, Object> paramaMap) {
+        StringBuilder sql = new StringBuilder(" from PatrolPotential where 1=1 ");
+        if (paramaMap.containsKey("code")) { //站点名称
+            sql.append(" and code =:code ");
+        }
+        super.pageByHql(sql.toString(), pageEntity, paramaMap);
+    }
 }
+
